4b89098cb20536f2bd359d74c40be7700b3d099a
Matheus <matheusousaa@gmail.com>
2025-07-16 11:56:47 -0300

    Solved FizzBuzz challenge with for loop

--- 01_03b/1-3-fizzbuzz.php
+++ 01_03b/1-3-fizzbuzz.php
@@ -1,1 +1,24 @@
 <?php
+/*CHALLENGE:
+Print numbers 1 to 100 (inclusive), while making important substitutions.
+
+REQUIREMENTS:
+1. Replace all multiples of 3 with the word "Fizz."
+2. Replace all multiples of 5 with the word "Buzz."
+3. Replace any numbers that are multiples of 3 and 5 with
+the word "FizzBuzz."
+*/
+
+for ($i=1; $i <= 100; $i++) {
+  if ($i % 3 == 0 && $i % 5 == 0) {
+    echo "FizzBuzz.";
+  } else if ($i % 3 == 0) {
+    echo "Fizz.";
+  } else if ($i % 5 == 0) {
+    echo "Buzz.";
+  } else {
+    echo $i;
+  }
+
+  echo "<br>";
+}
