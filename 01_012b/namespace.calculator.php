ea3822d924d0fcb25b6e49d1f04a11b8a176f643
Matheus <matheusousaa@gmail.com>
2025-07-18 11:25:26 -0300

    Created calculator namespace and updated all the scopes of the functions to receive array values

--- 01_012b/namespace.calculator.php
+++ 01_012b/namespace.calculator.php
@@ -1,25 +1,38 @@
 <?php
 
+namespace calculator;
+
 function add($numbers)
 {
+    return array_sum($numbers);
 }
 
 function subtract($start, $numbers)
 {
+    return $start - array_sum($numbers);
 }
 
 function multiply($numbers)
 {
+    return array_product($numbers);
 }
 
 function divide($start, $numbers)
 {
+    return $start / array_product($numbers);
 }
 
 function remainder($start, $numbers)
 {
+    foreach( $numbers as $num ) {
+        $start = $start % $num;
+        if ($start == 0) return $start;
+    }
+
+    return $start;
 }
 
 function average($numbers)
 {
+    return array_sum($numbers) / count($numbers);
 }
