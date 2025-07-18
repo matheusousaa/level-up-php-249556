d5f988c9c913c79b9402cbd658cc09267305f1e0
Matheus <matheusousaa@gmail.com>
2025-07-18 15:20:22 -0300

    Created game to guess a number correctly based on set cookie

--- 01_014b/1-14-guess-number.php
+++ 01_014b/1-14-guess-number.php
@@ -1,11 +1,20 @@
 <?php 
 
 function generate_number() {
-	//Fill this out
+	if (!isset( $_COOKIE['guess_this']) || is_null($_COOKIE['guess_this']))  {
+		setcookie('guess_this', rand(0, 50));
+	}
 } 
 
 function check_guess() {
-	// Fill this out
+	if ($_POST['guess'] == $_COOKIE['guess_this']) {
+		setcookie('guess_this', '', time() - 3600);
+		return "correct";
+	} else if ($_POST['guess'] > $_COOKIE['guess_this']) {
+		return "higher";
+	}
+
+	return "lower";
 }
 
 function test_game() {
@@ -15,6 +24,11 @@ function test_game() {
 	echo '</pre>';
 }
 
+$guess_answer = "";
+
+if (isset( $_POST['submit']) && isset($_COOKIE['guess_this'])) {
+	$guess_answer = check_guess();
+} 
 
 generate_number(); //This call will generate the number and store it in a cookie! 
 
@@ -29,11 +43,27 @@ generate_number(); //This call will generate the number and store it in a cookie
 	</head>
 	<body>
 		<main>
-			<!---Place the guess feedback / messages here-->
+			<?php 
+				switch ($guess_answer) {
+					case 'correct':
+						echo '<h3>You guessed right!!</h3>';
+						echo '<p><a href="'.$_SERVER['PHP_SELF'].'">Play again?</a></p>';
+						break;
+					case 'higher':
+						echo '<p>Your guess is higher</p>';
+						break;
+					case 'lower':
+						echo '<p>Your guess is lower</p>';
+						break;
+				}
+
+				if ('correct' !== $guess_answer) :
+			?>
 			<form name="guess" method="POST">
 					<input type="number" name="guess" min="0" max="50" placeholder="Enter your guess" />
 					<input type="submit" name="submit" value="Make Your Guess!" />
 			</form>	
+			<?php endif; ?>
 		</main>
 	</body>
 </html>
