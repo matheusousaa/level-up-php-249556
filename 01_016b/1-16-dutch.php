e4a53053617b7609098e7ae3b21a7bc1d2ed50b1
Matheus <matheusousaa@gmail.com>
2025-07-21 11:07:52 -0300

    Created solution for the National Dutch Flag Algorithm

--- 01_016b/1-16-dutch.php
+++ 01_016b/1-16-dutch.php
@@ -26,11 +26,19 @@ function print_array( $a ) {
 }
 
 function dutch_sort( $flag ) {
-	// Challenge: define this function
+	$r = [];
+    $w = [];
+    $b = [];
 
+	foreach ($flag as $marble) {
+		$$marble[] = $marble;
+	}
+
+	$dutch_flag = array_merge($r, $w, $b);
+	print_array($dutch_flag);
+
+	return $dutch_flag;
 }
 
 dutch_sort( generate_array(true) );
 dutch_sort( generate_array() );
-
-
