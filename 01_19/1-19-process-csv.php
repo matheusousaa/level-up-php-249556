d46fd9d45e504c49ec081aaca69112050b5b5a4a
Matheus <matheusousaa@gmail.com>
2025-07-23 15:07:33 -0300

    Created CSV to JSON convertion

--- 01_19/1-19-process-csv.php
+++ 01_19/1-19-process-csv.php
@@ -7,9 +7,36 @@ function print_array( $a ) {
 }
 
 function convert_csv_to_json( $file ) {
-	// Write this function
+	$csv_data = file_get_contents($file);
+	$rows = explode(PHP_EOL, $csv_data);
+	$columns = explode(',', $rows[0]);
+	$columns_total = count($columns);
+	$count = 0;
+	$data_array_to_convert = array();
+
+	for ($i=1; $i < count($rows); $i++) {
+		$row_array = array();
+
+		foreach (explode(',', $rows[$i]) as $row_data) {
+			if ($count == $columns_total) {
+				$count = 0;
+			}
+
+			$row_array[$columns[$count]] = $row_data;
+
+			if ($count == $columns_total-1) {
+				$data_array_to_convert[] = $row_array;
+				$row_array = array();
+			}
+
+			$count++;
+		}
+	}
+	return json_encode($data_array_to_convert);
 }
 
 $csv = 'customers.csv';
 
 $json = convert_csv_to_json( $csv );
+
+print_array($json);
